b9eeebe1dfeee8545b1decb10b37e0b08fb78ef7
David Reid <mackron@gmail.com>
2016-09-30 21:34:55 +1000

    Put length.hpp and charcount.hpp into a single file (size.hpp).

--- drsl/size.hpp
+++ drsl/size.hpp
@@ -1,13 +1,60 @@
-/**
-*   \file   charcount.h
-*   \author Dave Reid
-*   \brief  Header file for chrcount() implementations.
-*/
-#ifndef DRSL_CHARCOUNT
-#define DRSL_CHARCOUNT
+// Copyright (C) 2016 David Reid. See included LICENSE file.
 
 namespace drsl
 {
+/**
+*   \brief           Retrieves the length of a C style string.
+*   \param  str [in] The string to retrieve the length of.
+*   \return          The length of the C style string.
+*
+*   \remarks
+*       This function return the number of basic character units in the string. A
+*       "basic character unit" is a 'char' for char* strings and a 'wchar_t' in
+*       whcar_t* strings. The same sort of things applies for UTF strings.
+*       \par
+*       To retrieve the number of characters in the string insteaad of basic character
+*       units, use charcount() instead.
+*/
+template <typename T>
+inline size_t length(const T *str)
+{
+    assert(str != NULL);
+
+    const T *temp = str;
+
+    while (*temp != '\0')
+    {
+        ++temp;
+    }
+
+    return temp - str;
+}
+
+// Optimized cases.
+inline size_t length(const char *str)
+{
+    assert(str != NULL);
+
+    return ::strlen(str);
+}
+inline size_t length(const wchar_t *str)
+{
+    assert(str != NULL);
+
+    return ::wcslen(str);
+}
+
+template <typename T>
+inline size_t length(const reference_string<T> &str)
+{
+    assert(str.end >= str.start);
+
+    return str.end - str.start;
+}
+
+
+
+
 
 /**
 *   \brief                 Retrieves the number of characters in the given string.
@@ -51,7 +98,4 @@ inline size_t charcount(const reference_string<T> &str)
 {
     return charcount(str.start, str.end - str.start);
 }
-
 }
-
-#endif // DRSL_CHARCOUNT
